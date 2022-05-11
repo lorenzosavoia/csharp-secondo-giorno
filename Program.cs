b995a5f60998ea3e899d13069df6b2b527ee697b
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-11 13:06:11 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SecondoGiorno // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello, World!");
+
+            var nome = "marco";
+
+            Console.WriteLine(nome.GetType());
+
+            int iEtaDiMarco = 0;
+
+            Console.WriteLine("inserisci l'eta di marco:");
+
+            string sStringaInserita;
+            sStringaInserita = Console.ReadLine();
+            iEtaDiMarco = Convert.ToInt32(sStringaInserita);
+
+            int iNumeroDaRaddopiare = 9;
+            esercizi_secondogiorno.RaddoppiaNumero(ref iNumeroDaRaddopiare);
+            Console.WriteLine("il numeroi vale {0}", iNumeroDaRaddopiare);
+
+            string sAppo = "lorenzo";
+            esercizi_secondogiorno.ModificaStringa(ref sAppo);
+            Console.WriteLine("la stringa é {0}", sAppo);
+
+            //creo l'array
+            int[] ArrayDiNumeri;
+            //dichiaro la grandezza dell'array in questo caso ha 10 slot da 0 a 9
+            ArrayDiNumeri = new int[10];
+            //riempio il posto numero 3 dell'array con il numero 546
+            ArrayDiNumeri[3] = 546;
+
+            */
+
+            esercizi_secondogiorno.CalcoloMedia();
+        }
+    }
+}
---
+++ esercizi.cs
@@ -0,0 +1,62 @@
+﻿namespace SecondoGiorno
+{
+    public static class esercizi_secondogiorno
+
+    {
+        public static int PrimoEsercizio(int iPrimoNumero, int iSecondoNumero)
+        {
+            int iSomma;
+            iSomma = iPrimoNumero + iSecondoNumero;
+            return iSomma;
+        }
+
+        public static void RaddoppiaNumero(ref int iNumeroDaRaddoppiare)
+        {
+            iNumeroDaRaddoppiare = iNumeroDaRaddoppiare * 2;
+
+        }
+
+        public static void ModificaStringa(ref string sStringaDaModificare)
+        {
+            sStringaDaModificare = sStringaDaModificare + "adc";
+        }
+
+        public static float CalcolaMedia(int[] vVettoreInteri)
+        {
+            int iNumeriElementi = vVettoreInteri.Length;
+            float fSomma = 0;
+            for (int i = 0; i < iNumeriElementi; i++)
+            {
+                fSomma += vVettoreInteri[i];
+            }
+            fSomma = fSomma / (float)iNumeriElementi;
+
+            return fSomma;
+        }
+
+        public static void CalcoloMedia()
+        {
+            double dSomma = 0;
+            Console.WriteLine("di quanti numeri vuoi fare la media?");
+            double iNumeroMedia = Convert.ToDouble(Console.ReadLine());
+            for(int i = 0; i < iNumeroMedia; i++)
+            {
+                Console.WriteLine("inserisci numero:");
+                double dNumber = Convert.ToDouble(Console.ReadLine());
+                dSomma =+ dNumber; 
+            }
+            double dAverage = dSomma / iNumeroMedia;
+
+            Console.WriteLine("la media é: {0}", dAverage);
+            //return dAverage;
+
+        }
+        public static void CalcoloMediaArray()
+        {
+            double dAverageArray;
+
+
+        }
+    }
+}
+
